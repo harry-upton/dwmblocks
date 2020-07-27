8ab29f15123ab2765e94b863dec423a6ba13b144
OldMcSpud <harryupton100@gmail.com>
2020-07-27 15:22:11 +0100

    changed/added blocks

--- blocks.h
+++ blocks.h
@@ -1,12 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon,Command,Interval,Signal*/
-	
+
+	{"", "cputemp", 20, 0},
+	{"", "updatecount", 600, 0},
 	{"", "volumestat", 0, 10},
 	{"", "memory", 30, 0}, 
 	{"", "clock", 5, 0},
 	{"", "printdate", 5, 0},
-	{"", "updatecount", 600, 0},
+
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
